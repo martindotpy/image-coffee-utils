20c57366108e5ec31b2243ddfda250c4194f38b2
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-11-20 21:16:09 +0000

    FEAT: Add post create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+PROJECT_NAME="image-coffee-utils"
+PREFIX="$PROJECT_NAME-"
+
 # Config git
-git config --global --add safe.directory /workspaces/image-coffee-utils || echo "Error: git config"
+git config --global --add safe.directory /workspaces/$PROJECT_NAME
+git config devcontainers-theme.show-dirty 1
+
+# Config zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "export ZSH_THEME=\"refined\"" >> ~/.zshrc
+echo "plugins=(git, zsh-autosuggestions)" >> ~/.zshrc
 
 # Build java microservices
 ./mvnw clean install -DskipTests
+
+# Build python microservices
+python -m pip install --upgrade pip
+pip install poetry
+
+bash -c "cd /workspaces/${PROJECT_NAME}/${PREFIX}colors && poetry install"
