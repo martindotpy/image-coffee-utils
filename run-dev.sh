3b446e3f8fd80fda6575a59b9453067455b50bcb
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-11-22 20:44:14 -0500

    FIX: Fix git attributes

--- run-dev.sh
+++ run-dev.sh
@@ -3,26 +3,26 @@ PREFIX="image-coffee-utils-"
 
 # Run the config server
 echo "Running the config server: ${PREFIX}config"
-bash -c "./mvnw -f ${PREFIX}config/pom.xml clean install spring-boot:run -q -DskipTests"
+./mvnw -f ${PREFIX}config/pom.xml clean install spring-boot:run -q -DskipTests &
 
 sleep 6
 
 # Run the eureka server
 echo "Running the eureka server: ${PREFIX}eureka"
-bash -c "./mvnw -f ${PREFIX}eureka/pom.xml clean install spring-boot:run -q -DskipTests"
+./mvnw -f ${PREFIX}eureka/pom.xml clean install spring-boot:run -q -DskipTests &
 
 sleep 6
 
 # Run the gateway server
 echo "Running the gateway server: ${PREFIX}gateway"
-bash -c "./mvnw -f ${PREFIX}gateway/pom.xml clean install spring-boot:run -q -DskipTests"
+./mvnw -f ${PREFIX}gateway/pom.xml clean install spring-boot:run -q -DskipTests &
 
 sleep 6
 
 # Run the ui service
 echo "Running the ui service: ${PREFIX}ui"
-bash -c "./mvnw -f ${PREFIX}ui/pom.xml clean install spring-boot:run -q -DskipTests"
+./mvnw -f ${PREFIX}ui/pom.xml clean install spring-boot:run -q -DskipTests &
 
 # Run the colors service
 echo "Running the colors service: ${PREFIX}colors"
-bash -c "cd ${PREFIX}colors && poetry install && poetry run python app.py"
+cd ${PREFIX}colors && poetry install && poetry run python app.py &
