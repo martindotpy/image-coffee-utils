cb78b41b662e6d3b7afb5926d05406504cb7cfc1
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-12-02 21:43:11 -0500

    FEAT: Update resize controller

--- image-coffee-utils-resize/src/main/java/xyz/cupscoffee/imagecoffeeutils/resize/adapter/in/controller/ResizeController.java
+++ image-coffee-utils-resize/src/main/java/xyz/cupscoffee/imagecoffeeutils/resize/adapter/in/controller/ResizeController.java
@@ -1,12 +1,14 @@
 package xyz.cupscoffee.imagecoffeeutils.resize.adapter.in.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.Operation;
 import xyz.cupscoffee.imagecoffeeutils.resize.application.port.in.ResizePort;
 
 @RestController
@@ -15,14 +17,21 @@ public class ResizeController {
     @Autowired
     private ResizePort resizeService;
 
-    @PostMapping
+    /**
+     * Resizes the given image to the specified width and height.
+     *
+     * @param width  the desired width of the resized image
+     * @param height the desired height of the resized image
+     * @param image  the image file to be resized
+     * @return a ResponseEntity containing the resized image as a byte array
+     */
+    @Operation(summary = "Resize an image to the specified width and height")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<byte[]> resizeImage(
             @RequestParam int width,
             @RequestParam int height,
             @RequestParam MultipartFile image) {
         byte[] resizedImage = resizeService.resizeImage(width, height, image);
-        return ResponseEntity.ok()
-                .header("Content-Type", "image/jpeg")
-                .body(resizedImage);
+        return ResponseEntity.ok().header("Content-Type", "image/jpeg").body(resizedImage);
     }
 }
