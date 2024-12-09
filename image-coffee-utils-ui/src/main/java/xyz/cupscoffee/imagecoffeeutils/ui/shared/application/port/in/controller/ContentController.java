e4e1f5ee6aa85c24c9caed81656c5a4eb60276a1
ZoombieAlc <zoomiealc@gmail.com>
2024-12-09 00:18:19 -0500

    fixed menu

---
+++ image-coffee-utils-ui/src/main/java/xyz/cupscoffee/imagecoffeeutils/ui/shared/application/port/in/controller/ContentController.java
@@ -0,0 +1,20 @@
+package xyz.cupscoffee.imagecoffeeutils.ui.shared.application.port.in.controller;
+
+import java.io.Serializable;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+
+@Named
+@ViewScoped
+@Getter
+@Setter
+public class ContentController implements Serializable {
+    private String currentPage = "/components/organisms/colors.xhtml";
+
+    public void changePage(String page) {
+        this.currentPage = page;
+    }
+}
